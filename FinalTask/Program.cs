c6affafb7a9f0fb1853fcb9f00e161dc7b7cdd32
Иван Соколов <vano95.cokol@gmail.com>
2022-12-22 17:04:47 +0600

    create string array and show it, started to create 1st method

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string [] StartArray(int size)
+{
+  string [] startArray = new string[size];
+  for (int i = 0; i < size; i++)
+  {
+    Console.WriteLine($"Please, fill array{i}:\t");
+    startArray [i]= Convert.ToString(Console.ReadLine());
+  }
+  return startArray;
+
+
+}
+
+int CurrentSize (string [] array)
+{
+   int current = 0;
+   int i = 0;
+ while (i < array.Length)
+  {
+    if (array[i].Length <= 3)
+    {
+      current++;
+      i++;
+    }
+    else i++;
+  }
+  return current;
+}
+
+void Show2dArray(string [] array) // двумерного массива
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    {
+      Console.Write(array[i] + " "); // вывод значений
+    }
+    Console.WriteLine();
+}
+}
+
+
+
+
+
+
+
+
+Console.WriteLine("Imput the size of array" );
+int size = Convert.ToInt32(Console.ReadLine());
+/*
+Console.WriteLine("Please, fill array" );
+
+string a = Convert.ToString(Console.ReadLine());
+
+string b = Convert.ToString(Console.ReadLine());
+
+string c = Convert.ToString(Console.ReadLine());
+*/
+string [] newArray = StartArray(size);
+Show2dArray(newArray);
+
+CurrentSize(newArray);
